ab66edfaa6a7d62c78cacc38ebbc850f470cf9e7
Kyutae-Chae <ktchae83@gmail.com>
2022-06-13 16:32:04 +0900

    add : update & delete function

--- src/main/java/example/controller/ExampleRestController.java
+++ src/main/java/example/controller/ExampleRestController.java
@@ -32,15 +32,36 @@ public class ExampleRestController {
     @PostMapping("/content")
     @ResponseBody
     public BoardEntity createBoard(@RequestBody Board board) {
-        System.out.println("title : "+board.getTitle());
-        System.out.println("contents : "+board.getContents());
-        System.out.println("board.getCreateuser() = " + board.getCreateuser());
         BoardEntity b = new BoardEntity();
         b.setTitle(board.getTitle());
         b.setContents(board.getContents());
         b.setCreateuser(board.getCreateuser());
         boardRepository.save(b);
         return b;
+    }
+
+    @PatchMapping("/content/{boardId}")
+    @ResponseBody
+    public BoardEntity updateBoard(@RequestBody Board board, @PathVariable("boardId") long boardId) {
+        System.out.println("board.getTitle() = " + board.getTitle());
+        System.out.println("board.getContents() = " + board.getContents());
+        BoardEntity b = new BoardEntity();
+        b.setIdBoard(boardId);
+        b.setTitle(board.getTitle());
+        b.setContents(board.getContents());
+        b.setCreateuser(board.getCreateuser());
+        boardRepository.save(b);
+        return b;
+    }
+
+    @DeleteMapping("/content/{boardId}")
+    @ResponseBody
+    public boolean deleteBoard(@PathVariable("boardId") long boardId) {
+        if (boardRepository.findById(boardId).isPresent()) {
+            boardRepository.deleteById(boardId);
+            return true;
+        }
+        return false;
     }
 
 }
